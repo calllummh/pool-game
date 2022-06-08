6da3392a5bac594a3c71c1cbf730666649492b1b
Callum Hargrove <callum.hargrove21@imperial.ac.uk>
2022-06-08 02:19:16 +0200

    go

---
+++ web-app/browser/game.js
@@ -0,0 +1,22 @@
+//creating game object with three methods
+function Game(){
+
+}
+
+Game.prototype.init() = function(){
+    this.gameWorld = new this.gameWorld();
+
+}
+
+Game.prototype.start() = function(){
+    this.init();
+
+    this.
+}
+
+Game.prototype.mainLoop() = function(){
+    
+    .clear
+}
+
+let poolGame = new Game();
