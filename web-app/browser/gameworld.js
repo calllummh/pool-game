93e0fbfa0433e6a7fb5b16afcb7ac1b8c76867fb
Callum Hargrove <callum.hargrove21@imperial.ac.uk>
2022-06-08 02:41:28 +0200

    let's go

--- web-app/browser/gameworld.js
+++ web-app/browser/gameworld.js
@@ -1,13 +1,18 @@
 //function construtor containing all physical objects of game - responsible to update for every frame of game
 function gameWorld(){
+    this.stick = new Stick();
 
 }
 
 gameWorld.prototype.update(){
+
+    this.stick.update();
 
 }
 
 gameWorld.prototype.draw(){
 
     Canvas.drawimage(sprites.background, {x:0, y:0});
+
+    this.stick.draw
 }
