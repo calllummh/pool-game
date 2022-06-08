691410e79ea4ce073c04ba5d044808e6d2d0e860
Callum Hargrove <callum.hargrove21@imperial.ac.uk>
2022-06-08 02:18:56 +0200

    go for it

--- web-app/browser/assets.js
+++ web-app/browser/assets.js
@@ -2,27 +2,36 @@
 let sprites = {};
 let assetsStillLoading = 0
 
-//declaring function which activates callback function when assets are loaded
+//function to validate that all images are loading
 function assetsLoadingLoop(callback){
+
     if (assetsStillLoading){
         requestAnimationFrame(assetsLoadingLoop.bind(this,callback));
     } else{
         callback();
     }
+
 }
 
-
+//declaring function which activates callback function when assets are loaded
 function loadAssets(callback){
+
+    //function loading assets, creating image from different sprites (using path) 
     function loadSprite(fileName){
         assetsStillLoading++;
+
         let spriteImage = new Image():
         spriteImage.spr = "./assets/sprites/" + fileName;
+        
         spriteImage.onload = function(){
             assetsStillLoading--;
         }
 
+        //returning image
         return spriteImage;
     }
+    
+    //loading the background and stick
     sprites.background = loadSprite('spr_background.png')
     sprites.stick = loadSprite('spr_stick.png')
 
---
+++ web-app/browser/gameworld.js
@@ -0,0 +1,12 @@
+//function construtor containing all physical objects of game - responsible to update for every frame of game
+function gameWorld(){
+
+}
+
+gameWorld.prototype.update(){
+
+}
+
+gameWorld.prototype.draw(){
+    
+}
